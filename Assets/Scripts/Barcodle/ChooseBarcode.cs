d393531b63dbbce89edeebd7a73c51e22f5e784c
Dan5433 <grishkov.daniil@gmail.com>
2026-02-21 01:00:44 -0800

    Add barcode validation

--- Assets/Scripts/Barcodle/ChooseBarcode.cs
+++ Assets/Scripts/Barcodle/ChooseBarcode.cs
@@ -1,14 +1,40 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChooseBarcode : MonoBehaviour
 {
     [SerializeField] TMP_Text chosenBarcodeText;
+    [SerializeField] Button confirmButton;
+
+    const string placeholder = "--Bar-code--";
 
     public string ChosenBarcode => chosenBarcodeText.text;
+
+    private void OnEnable()
+    {
+        chosenBarcodeText.text = placeholder;
+        confirmButton.interactable = false;
+    }
 
     public void SetChosenBarcode(string barcode)
     {
         chosenBarcodeText.text = barcode;
+
+        confirmButton.interactable = IsBarcodeValid(barcode);
+    }
+
+    bool IsBarcodeValid(string barcode)
+    {
+        if (barcode.Length != 12)
+            return false;
+
+        foreach (char c in barcode)
+        {
+            if (!char.IsDigit(c))
+                return false;
+        }
+
+        return true;
     }
 }
