acb8923b4a427f00c5400b9d4c18b9f41ba0de29
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-02-20 20:58:48 -0800

    animations for scannable objets

--- Assets/Scripts/BarcodeMania/ScannableItemController.cs
+++ Assets/Scripts/BarcodeMania/ScannableItemController.cs
@@ -7,6 +7,47 @@ public class ScannableItemController : MonoBehaviour
     [SerializeField] private SpriteRenderer _SpriteRenderer;
     [SerializeField] private CircleCollider2D _Collider;
 
+    [Header("Floating")]
+    [SerializeField] private float minPushInterval = 1f;
+    [SerializeField] private float maxPushInterval = 3f;
+    [SerializeField] private float pushMagnitude = 0.5f;
+
+    private Rigidbody2D _rb;
+    private float _nextPushTime;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        if (_rb == null)
+            _rb = gameObject.AddComponent<Rigidbody2D>();
+
+        _rb.gravityScale = 0;
+        _rb.linearDamping = 0.5f;
+
+        ApplyRandomPush();
+        ScheduleNextPush();
+    }
+
+    private void Update()
+    {
+        if (Time.time >= _nextPushTime)
+        {
+            ApplyRandomPush();
+            ScheduleNextPush();
+        }
+    }
+
+    private void ScheduleNextPush()
+    {
+        _nextPushTime = Time.time + Random.Range(minPushInterval, maxPushInterval);
+    }
+
+    private void ApplyRandomPush()
+    {
+        Vector2 dir = Random.insideUnitCircle.normalized;
+        _rb.AddForce(dir * pushMagnitude, ForceMode2D.Impulse);
+    }
+
     public void SetScannableItem(ScannableItem item)
     {
         _ScannableItem = item;
