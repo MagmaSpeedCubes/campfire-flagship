6a6c5bc3d7709e7bd1a2e2831f200ff76ba372c2
Dan5433 <grishkov.daniil@gmail.com>
2026-02-20 22:40:47 -0800

    Add proper wordle checking with color

--- Assets/Scripts/Barcodle/InputAttempt.cs
+++ Assets/Scripts/Barcodle/InputAttempt.cs
@@ -5,12 +5,11 @@ public class InputAttempt : MonoBehaviour
 {
     [SerializeField] TMP_Text attemptPrefab;
     [SerializeField] Transform scrollViewContent;
-
-    const long testAnswer = 123_456_789_012;
+    [SerializeField] WordleCheck answerCheck;
 
     public void AddAttempt(string attempt)
     {
         TMP_Text newAttempt = Instantiate(attemptPrefab, scrollViewContent);
-        newAttempt.text = attempt;
+        newAttempt.text = answerCheck.Check(attempt);
     }
 }
---
+++ Assets/Scripts/Barcodle/WordleCheck.cs
@@ -0,0 +1,65 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WordleCheck : MonoBehaviour
+{
+    const string testAnswer = "012345678901";
+    char[] answerDigits;
+
+    private void Awake()
+    {
+        answerDigits = testAnswer.ToCharArray();
+    }
+
+    public string Check(string attempt)
+    {
+        Dictionary<char, int> answerDigitCounts = new();
+        foreach (char c in answerDigits)
+        {
+            if (answerDigitCounts.ContainsKey(c))
+                answerDigitCounts[c]++;
+            else
+                answerDigitCounts[c] = 1;
+        }
+
+        string[] coloredAttempt = new string[attempt.Length];
+
+        for (int i = 0; i < attempt.Length; i++)
+        {
+            char attemptDigit = attempt[i];
+
+            if (attemptDigit != answerDigits[i])
+                continue;
+
+            coloredAttempt[i] = ColorChar(attemptDigit, Color.limeGreen);
+            answerDigitCounts[attemptDigit]--;
+        }
+
+        for (int i = 0; i < attempt.Length; i++)
+        {
+            char attemptDigit = attempt[i];
+
+            if (coloredAttempt[i] != null || answerDigitCounts[attemptDigit] == 0 || !answerDigits.Contains(attemptDigit))
+                continue;
+
+            coloredAttempt[i] = ColorChar(attemptDigit, Color.gold);
+            answerDigitCounts[attemptDigit]--;
+        }
+
+        for (int i = 0; i < attempt.Length; i++)
+        {
+            if (coloredAttempt[i] != null)
+                continue;
+
+            coloredAttempt[i] = ColorChar(attempt[i], Color.dimGray);
+        }
+
+        return string.Join("", coloredAttempt);
+    }
+
+    string ColorChar(char c, Color color)
+    {
+        return $"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{c}</color>";
+    }
+}
