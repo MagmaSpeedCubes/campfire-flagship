b38d749b0c9f68680525d69883203c42a37831bc
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2026-02-21 07:39:59 -0800

    still working on rhythm

--- Assets/_RhythmSuperstore/Scripts/RhythmSuperstore_Controller.cs
+++ Assets/_RhythmSuperstore/Scripts/RhythmSuperstore_Controller.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 public class RhythmSuperstore_Controller : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private List<RhythmSuperstore_ItemController> _Items;
+    [SerializeField] private List<RhythmSuperstore_Item> _Items;
+    [SerializeField] private GameObject ItemPrefab;
+    [SerializeField] private Transform SpawnPosition;
+ 
+    [Header("Settings")]
+    [SerializeField] private float SpawnInterval;
+
+    [Header("Debug")]
+    [SerializeField] private float SpawnCounter;
+
+    private void Update()
+    {
+        SpawnCounter -= Time.deltaTime;
+        if (SpawnCounter <= 0)
+        {
+            SpawnCounter = SpawnInterval;
+            SpawnItem();
+        }
+    }
+
+    private void SpawnItem()
+    {
+        GameObject itemObj = Instantiate(ItemPrefab, SpawnPosition.position, Quaternion.identity, transform);
+        RhythmSuperstore_ItemController controller = itemObj.GetComponent<RhythmSuperstore_ItemController>();
+        int randIndex = Random.Range(0, _Items.Count);
+        controller.SetItem(_Items[randIndex]);
+    }
 }
