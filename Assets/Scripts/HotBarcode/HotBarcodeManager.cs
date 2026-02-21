7473088a91700526665a0f7263ba7799e791c23a
MagmaSpeedCubes <110320002+MagmaSpeedCubes@users.noreply.github.com>
2026-02-20 20:28:17 -0800

    added some functionality for HotBarcode

--- Assets/Scripts/HotBarcode/HotBarcodeManager.cs
+++ Assets/Scripts/HotBarcode/HotBarcodeManager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Collections;
 using TMPro;
 
 using UnityEngine;
@@ -10,9 +11,14 @@ public class HotBarcodeManager : MonoBehaviour
 
     int largestFound = 0;
     int activePlayer = 0;
+    List<int> playersInGame = new List<int>();
+    float timer = 0f;
+    [SerializeField] private float roundTime = 60f;
+    bool gameActive = false;
 
-    [SerializeField] private TextMeshProUGUI instructionText, actionText;
+    [SerializeField] private TextMeshProUGUI objectiveText, alertText, timerText;
     [SerializeField] private AudioClip success, failure, win, loss;
+    [SerializeField] private AudioClip backgroundMusic;
 
     private void Awake()
     {
@@ -27,19 +33,86 @@ public class HotBarcodeManager : MonoBehaviour
         }
     }
 
+    public void StartGame()
+    {
+        gameActive = true;
+        largestFound = 0;
+        activePlayer = 0;
+        timer = roundTime;
+        objectiveText.text = $"Player {activePlayer + 1} objective: Find a barcode";
+        alertText.text = "";
+
+        for (int i = 0; i < GameState.numPlayers; i++)
+        {
+            playersInGame.Add(i);
+        }
+        AudioSource asc = GetComponent<AudioSource>();
+        asc.clip = backgroundMusic;
+        asc.loop = true;
+        asc.Play();
+
+    }
+
+    void Update()
+    {
+        if (!gameActive) return;
+
+
+        timer -= Time.deltaTime;
+        timerText.text = $"Time: {Mathf.Ceil(timer)}s";
+        if(timer <= 0f)
+        {
+
+            StartCoroutine(FlashMessage(alertText, Color.red, $"Player {activePlayer + 1} ran out of time!", loss));
+            activePlayer = (activePlayer + 1) % GameState.numPlayers;
+            playersInGame.Remove(activePlayer);
+
+            if(playersInGame.Count == 1)
+            {
+                StartCoroutine(FlashMessage(alertText, Color.green, $"Player {playersInGame[0] + 1} wins!", win));
+                EndGame();
+            }
+            else
+            {
+                objectiveText.text = $"Player {activePlayer + 1} objective: Find a barcode";
+                timer = roundTime;
+            }   
+        }
+    }
+
+    public void EndGame()
+    {
+        gameActive = false;
+        objectiveText.text = "Game Over!";
+        alertText.text = "";
+        playersInGame.Clear();
+    }
+
     public void OnBarcodeFound(string text)
     {
         try
         {
             int barcodeValue = int.Parse(text);
-            //if(barcodeVal)
+            if(barcodeValue > largestFound)
+            {
+                StartCoroutine(FlashMessage(alertText, Color.green, $"Player {activePlayer + 1} found a new largest barcode: {barcodeValue}!", success));
+
+                largestFound = barcodeValue;
+                objectiveText.text = $"Player {activePlayer + 1} objective: Find a larger barcode than {largestFound}";
+                activePlayer = (activePlayer + 1) % GameState.numPlayers;
+                timer = roundTime;
+            }
+            else
+            {
+                StartCoroutine(FlashMessage(alertText, Color.yellow, $"{barcodeValue} is not larger than {largestFound}. Try again!", failure));
+            }
             
         }catch(System.Exception e)
         {
-            StartCoroutine(FlashMessage(actionText, Color.red, "Invalid barcode!", failure));
+            StartCoroutine(FlashMessage(alertText, Color.red, "Invalid barcode!", failure));
             return;
         }
-        activePlayer = (activePlayer + 1) % GameState.numPlayers;
+        
     }
 
     IEnumerator FlashMessage(TextMeshProUGUI text, Color color, string content, AudioClip clip)
